28bd0ed6b3a413eae39ff35156c83c06f87cda0d
Sebastián Otárola <sebastianotarolajara@gmail.com>
2025-08-09 16:33:14 -0600

    9-8-25 Habilitación de uso de certificados

--- src/config/db.js
+++ src/config/db.js
@@ -6,9 +6,9 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  //ssl: {
-    //rejectUnauthorized: true  // o false si no estás usando certificados verificados
-  //}
+  ssl: {
+    rejectUnauthorized: true  // o false si no estás usando certificados verificados
+  }
 });
 
 module.exports = db;
